6a24a46d5e495b7deabff3edd2c862cfcaa99048
Johny Alencar <johnyguido@hotmail.com.br>
2022-12-19 19:09:37 -0300

    Auditing data

--- backend/src/main/java/com/johny/dscatalog/entities/Category.java
+++ backend/src/main/java/com/johny/dscatalog/entities/Category.java
@@ -1,12 +1,10 @@
 package com.johny.dscatalog.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.time.Instant;
 
 @Builder
 @Data
@@ -22,5 +20,20 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
+
+    @PrePersist
+    public void prePersist(){
+        createdAt = Instant.now();
+    }
+    @PreUpdate
+    public void preUpdate(){
+        updatedAt = Instant.now();
+    }
 
 }
